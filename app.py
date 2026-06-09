40179883ff6ee86a0871713c8e88743de2a8ee90
bunnnnyyyy22 <125729816+bunnnnyyyy22@users.noreply.github.com>
2026-06-09 13:33:31 +0800

    Change rendered template from index.html to index1.html

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ client = OpenAI()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
